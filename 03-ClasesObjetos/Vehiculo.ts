eb22cc0b8af76efdac9a86fa8e970ac51a951a71
Daniel Fernando Yepez Velez <54162419+DanielFernandoYepezVelez@users.noreply.github.com>
2021-04-06 11:38:44 -0500

    Update Vehiculo.ts

--- 03-ClasesObjetos/Vehiculo.ts
+++ 03-ClasesObjetos/Vehiculo.ts
@@ -19,3 +19,12 @@ class Vehiculo {
 }
 
 const vehiculo = new Vehiculo('Ford', '12/07/2018', 4);
+
+/* Propiedades */
+console.log(vehiculo.marca);
+consoloe.log(vehiculo.fecha);
+console.log(vehiculo.puertas);
+
+/* Métodos */
+vehiculo.acelerar();
+vehiculo.frenar();
